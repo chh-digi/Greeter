0f79124364371fc2f428813bf952894a7fd44e7c
Hessam Hassanzadeh Ghalebsaz <hh@digisense.dk>
2018-07-09 13:17:16 +0200

    React with input type

--- src/greeter.tsx
+++ src/greeter.tsx
@@ -1,22 +1,33 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-interface IState {
+interface IProps {
+    lead: string,
     message: string
 }
 
-class Greeter extends React.Component<{}, IState> {
+interface IState {
+    tempInputValue: string,
+    toBeGreeted: string
+}
+
+class Greeter extends React.Component<IProps, IState> {
     constructor(props: any) {
         super(props);
 
-        this.state = { message: "Hello Christoffer! :)" }
-
-        console.log("I am alive...");
+        this.state = { toBeGreeted: "Christoffer", tempInputValue: "" }
     }
     
     render() {
-        return <div><input type='button' value="change" onClick={(e:any) => {this.setState({message: "Hello Hessam"})}}/><h1>{this.state.message}</h1></div>;
+        return <div>
+                    <h1>{this.props.lead}</h1>
+                    <hr />
+                    <br />
+                    <input type="button" value="Greet!" onClick={ (e:any) => { this.setState({ toBeGreeted: this.state.tempInputValue }) } }/>
+                    <input type="textbox" onChange={ (e:React.ChangeEvent<HTMLInputElement>) => { this.setState({ tempInputValue: e.target.value }) } } />
+                    <h3>{this.props.message.replace("{0}", this.state.toBeGreeted )}</h3>
+               </div>;
     }
 }
 
-ReactDOM.render(<Greeter />, document.getElementById('root'));
+ReactDOM.render(<Greeter lead={"Greetings!"} message="Welcome {0} :)" />, document.getElementById('root'));
